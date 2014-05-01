db76b8768ebb9026f1c60491f182b0fa45e3c536
Maikals <miquicm@gmail.com>
2014-05-01 11:34:33 +0200

    viewer modificat

--- viewer.sh
+++ viewer.sh
@@ -8,14 +8,14 @@ command_exists () {
 UNAME=`uname`
 
 if [  $UNAME = "Linux" ] ; then 
-    if command_exists chromium-browser ; then
+    if command_exists firefox ; then
+        firefox file://`pwd`/viewer.html?game=$1
+    elif command_exists chromium-browser ; then
         chromium-browser --kiosk --allow-file-access-from-files file://`pwd`/viewer.html?game=$1
     elif command_exists chrome-browser ; then
         chrome-browser  --kiosk --allow-file-access-from-files file://`pwd`/viewer.html?game=$1
     elif command_exists /opt/google/chrome/google-chrome ; then
         /opt/google/chrome/google-chrome --kiosk --allow-file-access-from-files file://`pwd`/viewer.html?game=$1
-    elif command_exists firefox ; then
-        firefox file://`pwd`/viewer.html?game=$1
     else
         echo "Cannot find a browser"
     fi
